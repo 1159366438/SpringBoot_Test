d13fa972eabee3b952ae5f795b53e2018bf868ca
ADMIN <1159366438@qq.com>
2026-03-03 01:30:35 +0800

    20260303 删除冗余代码

--- frontend/cock-vue3-vite/src/main.ts
+++ frontend/cock-vue3-vite/src/main.ts
@@ -4,11 +4,8 @@ import App from './App.vue'
 
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue' // 导入所有图标
-import axios from 'axios'
 import router from './router'
 import ElementPlus from 'element-plus' // 导入 Element Plus
-import 'element-plus/dist/index.css' // 导入样式
-import { ElMessage } from 'element-plus' // 导入 ElMessage
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -17,10 +14,6 @@ const pinia = createPinia()
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-
-// 全局挂载
-app.config.globalProperties.$axios = axios
-app.config.globalProperties.$message = ElMessage
 
 app.use(ElementPlus)
 app.use(router)
